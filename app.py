ad8d757037677b9a7081ecdfe31f10bb3479020e
nora chams <nchamseddin@gmail.com>
2023-02-21 16:11:44 -0500

    Add all users endpoints

--- app.py
+++ app.py
@@ -21,12 +21,14 @@ c.execute('''DROP TABLE IF EXISTS skills''')
 #Creating table for users
 c.execute('''
     CREATE TABLE users (
+        rowid INTEGER PRIMARY KEY,
         name TEXT,
         company TEXT,
         email TEXT,
         phone TEXT
     )
 ''')
+
           
 # Creating table for skills
 c.execute('''
@@ -57,3 +59,30 @@ conn.close()
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+# All Users Endpoint use localhost:5000/users/
+@app.route("/users")
+def get_users():
+    try:
+        conn = sqlite3.connect('users.db')
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        c.execute('SELECT * FROM users')
+        users = c.fetchall()
+        user_list = []
+        for user in users:
+            user_dict = dict(user)
+            c.execute('SELECT skill, rating FROM skills WHERE users_id = ?', (user_dict['rowid'],))
+            skills = c.fetchall()
+            skill_list = []
+            for skill in skills:
+                skill_dict = dict(skill)
+                skill_list.append(skill_dict)
+            user = {'name': user_dict['name'], 'email': user_dict['email'], 'skills': skill_list}
+            user_list.append(user)
+        conn.close()
+        response = {'users': user_list}
+        return json.dumps(response)
+    except Exception as e:
+        return f"An error occurred: {e}"
